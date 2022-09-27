03e3a8fd6080d763e9efde6fe085bd0824544244
Mohammad Almosallam <moh.almusallam4@gmail.com>
2022-09-27 21:47:32 +0300

    added goals controllers and routes

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log("tt");
+const { urlencoded } = require("body-parser");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/getRoutes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server running on PORT ${port}`);
+});
