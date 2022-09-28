9dcd5f36abd70c7b9a3bc1154e6f638d332de05d
Mohammad Almosallam <moh.almusallam4@gmail.com>
2022-09-28 21:10:57 +0300

    added userModel,userRoutes,userControllers,authenticate user via JWT, authMiddleware,protect goal routes

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,12 +1,14 @@
 const asyncHandler = require("express-async-handler");
 
 const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 
 // @desc    Get Goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  //req.user.id is accesed after autherization thru authMiddleware
+  const goals = await Goal.find({ user: req.user.id });
 
   res.status(200).json(goals);
 });
@@ -20,8 +22,10 @@ const setGoal = asyncHandler(async (req, res) => {
     throw new Error("Please add a text field");
   }
 
+  //req.user.id is accesed after autherization thru authMiddleware
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
 
   res.status(200).json(goal);
@@ -36,6 +40,18 @@ const updateGoal = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+  //Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
 
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
@@ -55,6 +71,19 @@ const deleteGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Goal not found");
   }
+
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+  //Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
   await goal.remove();
 
   res.status(200).json({ id: req.params.id });
--- server/models/goalModel.js
+++ server/models/goalModel.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 
 const goalSchema = mongoose.Schema(
   {
+    //To know what goal belongs to what user.
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     text: {
       type: String,
       required: [true, "Please add a text value"],
